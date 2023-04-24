a072197de1c5dbe4343b4dbfa38bae4e146fceb9
dotuser <ahmedhassaan@tutanota.com>
2023-04-24 23:24:50 +0500

    Connected to DataBase && Finalized

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -13,17 +14,29 @@ const contactContent =
 const app = express();
 const PORT = 3000;
 
-const posts = [];
-
 app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const uri =
+  "mongodb+srv://hassan:Testing123@generalpurpose.bxoapvc.mongodb.net/blogDB?retryWrites=true&w=majority";
+
+mongoose.connect(uri, { useNewUrlParser: true });
+
+const postSchema = {
+  title: String,
+  content: String,
+};
+
+const Post = mongoose.model("Post", postSchema);
+
 app.get("/", (req, res) => {
-  res.render("home", {
-    homeStartingContent: homeStartingContent,
-    blogs: posts,
+  Post.find().then((posts) => {
+    res.render("home", {
+      homeStartingContent: homeStartingContent,
+      posts: posts,
+    });
   });
 });
 
@@ -40,26 +53,21 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  const post = {
+  const post = new Post({
     title: req.body.title,
     content: req.body.content,
-  };
-  posts.push(post);
+  });
+  post.save();
   res.redirect("/");
 });
 
-app.get("/posts/:postName", (req, res) => {
-  const requestedPost = _.lowerCase(req.params.postName);
-
-  posts.forEach((blog) => {
-    const blogTitle = _.lowerCase(blog.title);
-
-    if (blogTitle === requestedPost) {
-      res.render("post", {
-        title: blog.title,
-        content: blog.content,
-      });
-    }
+app.get("/posts/:postId", (req, res) => {
+  const requestedPostId = req.params.postId;
+  Post.findOne({ _id: requestedPostId }).then((post) => {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
+    });
   });
 });
 
